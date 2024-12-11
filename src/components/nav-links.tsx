00a9ef8ae1b88d45337d536a07711bbdd27c710a
TharishaPerera <tharishaperera@gmail.com>
2024-12-11 23:13:47 +0530

    added active link

--- src/components/nav-links.tsx
+++ src/components/nav-links.tsx
@@ -1,20 +1,31 @@
-import React from "react"
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 import { navLinks } from "../constants/nav-links";
 
 interface NavLinksProps {
-    className?: string
+  className?: string;
 }
 
 export const NavLinks: React.FC<NavLinksProps> = ({ className }) => {
-    return (
-      <ul className={className}>
-        {navLinks.map((link, index) => (
-          <Link key={index} to={link.path} className="font-semibold hover:text-gray-600 transition">
+  const location = useLocation();
+
+  return (
+    <ul className={className}>
+      {navLinks.map((link, index) => {
+        const isActive = location.pathname === link.path;
+        return (
+          <Link
+            key={index}
+            to={link.path}
+            className={`font-semibold transition ${
+              isActive ? "underline underline-offset-4" : "hover:text-gray-600 "
+            }`}
+          >
             {link.name}
           </Link>
-        ))}
-      </ul>
-    );
-}
+        );
+      })}
+    </ul>
+  );
+};
